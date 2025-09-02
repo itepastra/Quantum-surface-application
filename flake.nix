3c783b583f41527848dd53c8fc4de1f58eecdf67
Noa Aarts <noa@voorwaarts.nl>
2025-09-02 09:57:34 +0200

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+  outputs =
+    { self, nixpkgs, ... }:
+    {
+      devShells."x86_64-linux".default =
+        let
+          pkgs = import nixpkgs { system = "x86_64-linux"; };
+        in
+        pkgs.mkShell {
+          packages = [
+            pkgs.dotnetCorePackages.dotnet_9.sdk
+            pkgs.nuget
+          ];
+        };
+    };
+}
