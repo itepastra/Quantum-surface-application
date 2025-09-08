f072de59fb762e9d3ec3aa79233afb4e946b4f20
Noa Aarts <noa@voorwaarts.nl>
2025-09-08 18:54:07 +0200

    fix qubit-quilt-game build

--- flake.nix
+++ flake.nix
@@ -5,6 +5,10 @@
       url = "github:getzola/book";
       flake = false;
     };
+    export_templates = {
+      url = "https://github.com/godotengine/godot/releases/download/4.4.1-stable/Godot_v4.4.1-stable_export_templates.tpz";
+      flake = false;
+    };
   };
 
   outputs =
@@ -29,8 +33,43 @@
         in
         rec {
           default = qubit-quilt-site;
-          qubit-quilt-site = pkgs.stdenv.mkDerivation {
+          qubit-quilt-game = pkgs.stdenv.mkDerivation {
             name = "qubit-quilt";
+            nativeBuildInputs = [
+              pkgs.godot
+              pkgs.unzip
+            ];
+
+            src = fs.toSource {
+              root = ./.;
+              fileset = fs.union (fs.difference (fs.difference ./qubit-quilt (fs.maybeMissing ./qubit-quilt/.godot)) (fs.maybeMissing ./qubit-quilt/export)) ./assets;
+            };
+
+            buildPhase = ''
+              export HOME=$(pwd)
+              mkdir -p $HOME/.local/share/godot/export_templates/4.4.1.stable
+              pushd $HOME/.local/share/godot/export_templates/4.4.1.stable
+              unzip -j ${inputs.export_templates} \
+                templates/web_dlink_nothreads_debug.zip \
+                templates/web_nothreads_debug.zip \
+                templates/web_dlink_nothreads_release.zip \
+                templates/web_nothreads_release.zip
+              popd
+              pushd qubit-quilt
+              mkdir export
+              godot --verbose --headless --export-release Web export/qubit-quilt.html
+              popd
+            '';
+
+            installPhase = ''
+              mkdir -p $out
+              pushd qubit-quilt
+              cp export/* $out
+              popd
+            '';
+          };
+          qubit-quilt-site = pkgs.stdenv.mkDerivation {
+            name = "qubit-quilt-site";
 
             nativeBuildInputs = [
               pkgs.zola
