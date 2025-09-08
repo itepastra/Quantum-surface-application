a7bd4b8d406eff5a1fa632f090cfcb14b3a49204
Noa Aarts <noa@voorwaarts.nl>
2025-09-08 17:15:19 +0200

    add theme as a submodule

--- flake.nix
+++ flake.nix
@@ -15,5 +15,29 @@
             pkgs.zola
           ];
         };
+      packages."x86_64-linux" =
+        let
+          pkgs = import nixpkgs { system = "x86_64-linux"; };
+          fs = pkgs.lib.fileset;
+        in
+        rec {
+          default = qubit-quilt-site;
+          qubit-quilt-site = pkgs.stdenv.mkDerivation {
+            name = "qubit-quilt";
+
+            nativeBuildInputs = [
+              pkgs.zola
+            ];
+
+            src = fs.toSource {
+              root = ./website;
+              fileset = fs.difference ./website (fs.maybeMissing ./website/public);
+            };
+
+            buildPhase = ''
+              zola build --output-dir $out
+            '';
+          };
+        };
     };
 }
