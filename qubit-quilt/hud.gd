0e4173551860b7eacdebf9ed30f7fe8cda25035b
Noa Aarts <noa@voorwaarts.nl>
2025-09-22 09:46:33 +0200

    clean up icon and code and add comments

--- qubit-quilt/hud.gd
+++ qubit-quilt/hud.gd
@@ -6,15 +6,17 @@ var codeEditor: CodeEdit
 var codeToggle: Button
 
 func _ready() -> void:
-	print_debug(vport)
+	# when the viewport changes size we need to move the editor around
 	vport.size_changed.connect(_on_size_changed)
 	
 	codeEditor = get_node("./CodeEdit")
 	codeToggle = get_node("./CodeEditor")
+	
+	# place the button and editor in the correct spot at initialisation
+	_on_size_changed()
 
 func _on_size_changed():
 	var rect = vport.get_visible_rect()
-	print_debug("size changed to", rect)
 	codeEditor.size = rect.size * Vector2(0.4, 0.8) - Vector2(8, 8)
 	codeEditor.position = rect.position + rect.size * Vector2(0.6, 0.0) + Vector2(0, 48)
 	codeToggle.position = rect.position + rect.size * Vector2(1,0) + Vector2(-codeToggle.size.x - 8, 8)
