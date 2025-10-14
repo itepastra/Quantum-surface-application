3de23f64574ae27f3128c583629605dc6d74935d
Noa Aarts <noa@voorwaarts.nl>
2025-10-14 17:07:07 +0200

    fix mathml notations

--- flake.nix
+++ flake.nix
@@ -65,7 +65,6 @@
               cp -r . $out
               cp ${./assets/favicon.png} $out/favicon.png
               cp ${./assets/QQ-icon-full.png} $out/qq.png
-              cp -r ${./website/static} $out/static
 
               ${pkgs.imagemagick}/bin/convert -resize x16 -gravity center -crop 16x16+0+0 ${./assets/favicon.png} -flatten -colors 256 -background transparent $out/favicon.ico
             '';
